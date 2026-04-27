8ffb2898a2b76e26fa876e5435e1ed34126ee5a0
ermadatov <emilis.highscreen@gmail.com>
2026-04-27 16:37:08 +0300

    fix 0.0.0.0

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = create_app()
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        host=os.environ.get("HOST", "127.0.0.1"),
+        host=os.environ.get("HOST", "0.0.0.0"),
         port=int(os.environ.get("PORT", "8000")),
         reload=bool(os.environ.get("RELOAD")),
     )
